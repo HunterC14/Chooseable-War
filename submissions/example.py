8cb1765c04fc7df421ab2eefb69133c2bdc405ad
Karl Chen <quarl@quarl.org>
2026-07-04 18:14:29 -0400

    improve example

--- submissions/example.py
+++ submissions/example.py
@@ -23,9 +23,12 @@ class example:
         # print(f"Bounty: {bounty} cont. ({__name__} g#{my_id})")
         # print(f"Warring with number {ofnum} cont.")
         # print(f"All cards: {cards} cont.")
-        random.shuffle(self.hand)
-        # print(f"Choosing cards for war: {hand[:4]} end")
-        return tuple(self.hand[:4]) # first int is the played card, the one that is actually compared.
+        play = random.choice(self.hand)
+        rest = sorted(self.hand)
+        rest.remove(play)
+        # print(f"Choosing cards for war: {(play, *rest[:3])} end")
+        return (play, *rest[:3]) # first int is the played card, the one that is actually compared.
                             # other 3 are the "bounty" cards which don't affect the outcome but lets the winner win more cards.
+                            # always sacrifice the 3 lowest remaining cards.
 
 bot=example
